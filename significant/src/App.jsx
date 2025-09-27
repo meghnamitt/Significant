a9ed71343be564574d6d55d3f9f4e99e2658f884
druiz365 <druiz35@gatech.edu>
2025-09-27 11:45:04 -0400

    RefSearch first commit
    Added skeleton of ReferenceSearch

--- significant/src/App.jsx
+++ significant/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
+import ReferenceSearch from './pages/ReferenceSearch';
 import './App.css'
 
 function App() {
@@ -10,11 +11,13 @@ function App() {
       <div className="App">
         <nav style={{ marginBottom: '1rem' }}>
           <Link to="/">Home</Link> |{' '}
-          <Link to="/quiz">Quiz</Link>
+          <Link to="/quiz">Quiz</Link> |{' '}
+          <Link to="/reference-search">Reference Search</Link>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/reference-search" element={<ReferenceSearch />} />
         </Routes>
       </div>
     </Router>
