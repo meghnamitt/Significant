20aeb4944da611849adcacc03750f60b2b70652e
druiz365 <druiz35@gatech.edu>
2025-09-27 15:12:33 -0400

    nothing
    nothing

--- significant/src/pages/HomePage.jsx
+++ significant/src/pages/HomePage.jsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <div>
       <h1>Welcome to SIGNificant</h1>
       <p>Your ASL learning platform</p>
-      <div style={{ marginTop: 20 }}>
-        <Link to="/login">Login</Link>
+      <div style={{ marginTop: 24 }}>
+        <Link to="/login">Login to store your data</Link>
       </div>
     </div>
   );
