8d29ebacae7dd555cfe047e9b118f7887118f2e6
agopal45 <agopal45@gatech.edu>
2025-09-27 12:21:13 -0400

    video-recorder

--- significant/src/App.jsx
+++ significant/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
+import VideoQuiz from './pages/VideoQuiz';
 import './App.css'
 
 function App() {
@@ -10,11 +11,13 @@ function App() {
       <div className="App">
         <nav style={{ marginBottom: '1rem' }}>
           <Link to="/">Home</Link> |{' '}
-          <Link to="/quiz">Quiz</Link>
+          <Link to="/quiz">Quiz</Link> |{' '}
+          <Link to="/video-quiz">Video Quiz</Link>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/video-quiz" element={<VideoQuiz />} />
         </Routes>
       </div>
     </Router>
