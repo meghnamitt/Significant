8b325d87c6ff03d870fbd194919957de6206ba6d
agopal45 <agopal45@gatech.edu>
2025-09-26 21:18:08 -0400

    create-progess-bar

--- significant/src/App.jsx
+++ significant/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Template from './components/Template'
+import ProgressBar from './components/ProgressBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,6 +34,7 @@ function App() {
       >
         <p>This is some content passed as children into the Template.</p>
       </Template>
+      <ProgressBar></ProgressBar>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
